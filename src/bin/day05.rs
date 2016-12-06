63e24d6909ac7099fb156ffc1787189167c33648
Chris Leech <cleech@redhat.com>
2016-12-06 11:42:52 -0800

    multi-threaded day 5

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,88 +1,118 @@
+#![feature(step_by)]
+
 use std::io;
 use std::io::Write;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 extern crate crypto;
-use self::crypto::digest::Digest;
-use self::crypto::md5::Md5;
+use crypto::digest::Digest;
+use crypto::md5::Md5;
 
 extern crate itertools;
-use self::itertools::unfold;
+use itertools::unfold;
 
 extern crate rustc_serialize;
-pub use self::rustc_serialize::hex::*;
+pub use rustc_serialize::hex::*;
+
+extern crate arrayvec;
+use arrayvec::ArrayVec;
+
+extern crate num_cpus;
 
 const DATA: &'static str = include_str!("day05.txt");
 
 pub fn main() {
     let input = DATA.trim();
 
-    let code = unfold(0, |mut idx| Some(find_interesting_hash(input, &mut idx, 5)))
-        .map(|hash| hash.chars().skip(5).next().unwrap())
+    let code = unfold(0, |mut idx| {
+            Some(find_interesting_hash(input, &mut idx, 1))
+        })
+        .map(|(_, hash)| hash.chars().skip(5).next().unwrap())
         .take(8)
         .collect::<String>();
 
     println!("day05 part1: {}", code);
 
-    let mut hashes = unfold(0, |mut idx| Some(find_interesting_hash(input, &mut idx, 5)));
-    let mut c2: Vec<u8> = vec![b'_'; 8];
-    print!("day05 part2: {}", unsafe { String::from_utf8_unchecked(c2.clone()) });
+    print!("day05 part2: --------");
     io::stdout().flush().unwrap();
-    loop {
-        let hash = hashes.next().unwrap();
-        let mut ch = hash.chars().skip(5);
-        let n = ch.next().unwrap().to_digit(16).unwrap() as usize;
-        let b = ch.next().unwrap().to_digit(16).unwrap() as u8;
 
-        if n < 8 && c2[n] == b'_' {
-            c2[n] = [b].to_hex().bytes().skip(1).next().unwrap() as u8;
-            print!("\rday05 part2: {}", unsafe { String::from_utf8_unchecked(c2.clone()) });
-            io::stdout().flush().unwrap();
+    let code2 = Arc::new(Mutex::new((vec![u32::max_value(); 8], vec![b'-'; 8])));
+    let cpus = num_cpus::get();
+    let mut threads = Vec::with_capacity(cpus);
 
-            if c2.iter().all(|b| *b != b'_') {
-                break;
+    for cpu in 0..cpus {
+        let code2 = code2.clone();
+
+        let tid = thread::spawn(move || {
+            let mut hashes = unfold(cpu as u32, |mut idx| {
+                Some(find_interesting_hash(input, &mut idx, cpus as u32))
+            });
+
+            loop {
+                let (idx, hash) = hashes.next().unwrap();
+                let mut ch = hash.bytes().skip(5);
+                let n = ch.next().unwrap().to_digit_dumb().unwrap() as usize;
+                if n > 7 {
+                    continue;
+                }
+                let b = ch.next().unwrap();
+
+                let mut code2 = code2.lock().unwrap();
+                // locked section from here to end of loop
+                if code2.0[n] > idx {
+                    code2.0[n] = idx;
+                    code2.1[n] = b;
+                    print!("\rday05 part2: {}", unsafe { String::from_utf8_unchecked(code2.1.clone()) });
+                    io::stdout().flush().unwrap();
+                }
+                if code2.0.iter().all(|n| *n < idx) {
+                    break;
+                }
             }
+        });
+        threads.push(tid);
+    }
+    for tid in threads { let _ = tid.join(); }
+
+    let code2 = code2.lock().unwrap();
+    println!("\rday05 part2: {}", unsafe { String::from_utf8_unchecked(code2.1.clone()) });
+}
+
+fn leading_zeros(buf: &[u8]) -> bool {
+    buf[0] | buf[1] | (buf[2] >> 4) == 0
+}
+
+trait DumbConvert {
+    fn to_digit_dumb(&self) -> Option<u8>;
+}
+impl DumbConvert for u8 {
+    fn to_digit_dumb(&self) -> Option<u8> {
+        match *self {
+            b'0'...b'9' => Some(*self - b'0'),
+            b'a'...b'f' => Some(*self - b'a' + 10),
+            _ => None,
         }
     }
-    println!("\rday05 part2: {}", unsafe { String::from_utf8_unchecked(c2.clone()) });
 }
 
-fn leading_zeros(buf: &[u8], count: usize) -> bool {
-    let (bytes, nibble) = (count / 2, count % 2);
-    buf[..bytes].iter().all(|b| *b == 0) &&
-    if nibble != 0 {
-        (buf[bytes] & 0xf0) == 0
-    } else {
-        true
-    }
-}
-
-fn find_interesting_hash(key: &str, start: &mut u32, zeros: usize) -> String {
+fn find_interesting_hash(key: &str, start: &mut u32, stride: u32) -> (u32, String) {
     let mut md5 = Md5::new();
-    let k = key.as_bytes();
-    let mut buf = Vec::with_capacity(256);
+    let mut buf = ArrayVec::<[_; 64]>::new();
     let mut output = [0u8; 16];
 
-    *start = (*start..)
+    *start = (*start..).step_by(stride)
         .find(|n| {
             md5.reset();
-            md5.input(k);
             buf.clear();
-            write!(&mut buf, "{}", n).unwrap();
+            write!(&mut buf, "{}{}", key, n).unwrap();
             md5.input(&buf);
             md5.result(&mut output);
-            leading_zeros(&output, zeros)
+            leading_zeros(&output)
         })
         .unwrap();
-    // println!("\t{}", start);
-    *start += 1;
 
-    output.to_hex()
-}
-
-#[cfg(test)]
-mod test {
-    use super::find_interesting_hash;
-
-    #[test]
-    fn examples() {}
+    let res = (*start, output.to_hex());
+    *start += stride;
+    res
 }
